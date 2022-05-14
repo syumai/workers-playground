509888e902220fd271bfb743f23bc470ab3e3c74
syumai <syumai@gmail.com>
2022-05-14 23:09:03 +0900

    stop caching 404

--- r2-image-viewer/src/index.ts
+++ r2-image-viewer/src/index.ts
@@ -21,21 +21,19 @@ async function handleRequest(event: FetchEvent): Promise<Response> {
   }
   const imgObj = await BUCKET.get(imgPath);
 
-  let res: Response;
   if (imgObj === null) {
-    res = new Response("Not found", {
+    return new Response("Not found", {
       status: 404,
     });
-  } else {
-    res = new Response(imgObj.body, {
-      headers: {
-        "Cache-Control": "public, max-age=14400",
-        Expires: "",
-        "Content-Type":
-          imgObj.httpMetadata.contentType ?? "application/octet-stream",
-      },
-    });
   }
+  const res = new Response(imgObj.body, {
+    headers: {
+      "Cache-Control": "public, max-age=14400",
+      Expires: "",
+      "Content-Type":
+        imgObj.httpMetadata.contentType ?? "application/octet-stream",
+    },
+  });
   event.waitUntil(cache.put(req.url, res.clone()));
 
   return res;
