1d2b1e9abb70d92c42cc6013471537ed49e1b20e
syumai <syumai@gmail.com>
2022-05-15 21:37:55 +0900

    add 400 to tinygo example

--- tinygo-add/worker.mjs
+++ tinygo-add/worker.mjs
@@ -13,8 +13,13 @@ export default {
   async fetch(req) {
     const instance = await load;
     const url = new URL(req.url);
-    const a = url.searchParams.get("a") ?? 1;
-    const b = url.searchParams.get("b") ?? 1;
+    const a = url.searchParams.get("a");
+    const b = url.searchParams.get("b");
+    if (a === null || b === null) {
+      return new Response("add url params like `?a=1&b=2` to results `1 + 2 = 3`", {
+        status: 400
+      })
+    }
     const result = `${a} + ${b} = ${instance.exports.add(a, b)}`;
     return new Response(result);
   },
