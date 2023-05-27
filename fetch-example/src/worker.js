69d87180f3062c49f527f7cf16db57a2c1dc8d60
syumai <syumai@gmail.com>
2023-05-27 12:32:23 +0900

    add fetch example

---
+++ fetch-example/src/worker.js
@@ -0,0 +1,17 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx) {
+    const resp = await fetch("https://syum.ai")
+    const body = await resp.text()
+		return new Response(body);
+	},
+};
