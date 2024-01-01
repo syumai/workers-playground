0828c616610cadff6c09b4d4be534fe56e4500b6
syumai <syumai@gmail.com>
2024-01-02 00:13:26 +0900

    add date handler example to tinygo-date-example

--- tinygo-date-example/main.go
+++ tinygo-date-example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"syscall/js"
 	"time"
 
 	"github.com/syumai/workers"
@@ -10,6 +11,11 @@ import (
 func main() {
 	http.HandleFunc("/now", func(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte(time.Now().String()))
+	})
+	http.HandleFunc("/date", func(w http.ResponseWriter, req *http.Request) {
+		ms := js.Global().Get("Date").Call("now").Float()
+		now := time.UnixMilli(int64(ms))
+		w.Write([]byte(now.String()))
 	})
 	workers.Serve(nil) // use http.DefaultServeMux
 }
