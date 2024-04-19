3877c5bac5c6e747b7b5ff258c18d71ff3ace5a9
syumai <syumai@gmail.com>
2024-04-20 03:36:07 +0900

    add reproduction code of issue 103

--- syumai-workers-repro-issue-103/go.mod
+++ syumai-workers-repro-issue-103/go.mod
@@ -4,4 +4,22 @@ go 1.21.3
 
 toolchain go1.22.1
 
-require github.com/syumai/workers v0.24.0
+require (
+	github.com/klauspost/compress v1.17.8
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/syumai/workers v0.24.0
+)
+
+require (
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
+
+replace github.com/syumai/workers => ../../workers
--- syumai-workers-repro-issue-103/main.go
+++ syumai-workers-repro-issue-103/main.go
@@ -1,24 +1,83 @@
+// based on: https://github.com/syumai/workers/issues/103 by @eliezedeck
 package main
 
 import (
 	"bytes"
+	"compress/flate"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 
+	"github.com/klauspost/compress/zip"
+	"github.com/labstack/echo/v4"
 	"github.com/syumai/workers"
+	"github.com/syumai/workers/cloudflare"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		msg := "Hello!"
-		w.Write([]byte(msg))
-	})
-	http.HandleFunc("/echo", func(w http.ResponseWriter, req *http.Request) {
-		b, err := io.ReadAll(req.Body)
-		if err != nil {
-			panic(err)
+	bucket, err := cloudflare.NewR2Bucket("DOCUMENTS")
+	if err != nil {
+		log.Fatalf("Error creating Cloudflare R2 bucket: %s", err)
+	}
+
+	e := echo.New()
+
+	e.GET("/services/stream-zip-from-r2", func(c echo.Context) error {
+		// Start streaming
+		c.Response().Header().Set(echo.HeaderContentType, "application/zip")
+		c.Response().Header().Set(echo.HeaderContentDisposition, "attachment; filename=example.zip")
+		c.Response().WriteHeader(http.StatusOK)
+
+		// Prepare to stream a ZIP file from R2
+		var zipBuf bytes.Buffer
+		writer := zip.NewWriter(&zipBuf)
+		writer.RegisterCompressor(zip.Deflate, func(out io.Writer) (io.WriteCloser, error) {
+			return flate.NewWriter(out, flate.NoCompression)
+		})
+
+		files := []string{
+			"akiba.jpg",
+			"text1.txt",
+			"text2.txt",
+			"text3.txt",
+			"text4.txt",
+			"text5.txt",
 		}
-		io.Copy(w, bytes.NewReader(b))
+		for _, file := range files {
+			fmt.Println("run: ", file)
+			// Get the file from R2
+			obj, err := bucket.Get(file)
+			if err != nil {
+				c.Logger().Errorf("failed to get file '%s' from R2: %v", file, err)
+				return err
+			}
+			if obj == nil {
+				c.Logger().Errorf("file '%s' is nil", file)
+				return nil
+			}
+			fileWriter, err := writer.Create(file)
+			if err != nil {
+				c.Logger().Errorf("failed to create file '%s' in ZIP: %v", file, err)
+				return err
+			}
+
+			if _, err := io.Copy(fileWriter, obj.Body); err != nil {
+				c.Logger().Errorf("failed to copy file '%s' from R2: %v", file, err)
+				return err
+			}
+		}
+		if err := writer.Flush(); err != nil {
+			c.Logger().Errorf("Error flushing zip writer: %s", err)
+		}
+		if err := writer.Close(); err != nil {
+			c.Logger().Errorf("failed to close ZIP writer: %v", err)
+		}
+		if _, err := io.Copy(c.Response(), &zipBuf); err != nil {
+			c.Logger().Error("failed to copy zip from buffer")
+			return err
+		}
+		return nil
 	})
-	workers.Serve(nil) // use http.DefaultServeMux
+	workers.Serve(e)
 }
